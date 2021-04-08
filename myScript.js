260f7d46373685eaf9ff44c47d4a361c38799490
Cordovani <lor.cordovani@gmail.com>
2021-04-08 15:31:19 +0200

    implementazione condizioni per lo sconto

--- myScript.js
+++ myScript.js
@@ -7,8 +7,25 @@
     // controllo dell'input 
     
 
-    // calcolo per i prezzi 
+    // calcolo per i prezzi 0.21€/km
+    var prezzo = kmPercorso  * 0.21;
 
+    console.log(prezzo);
+
+    // applica uno sconto in base all'età dell'utente
+    // 40% per i minorenni. 
+    // 20% per gli over 65.
+
+    var prezzoScontato = 0;  
+
+    if ( età >= 18 && età <= 65 ){
+        prezzoScontato = prezzo;
+    } else if (età < 18){
+        // sconto del 40%
+    } else if (età > 65){
+        // sconto del 20%
+    }
+    
 
     // output del programma
 
